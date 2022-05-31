008d3dfa89d3adc344a9519290a058ea70c1d250
Amadaro <amadarog@gmail.com>
2022-05-31 22:51:55 +0300

    Button and nav animation

--- js/map.js
+++ js/map.js
@@ -1,22 +1,38 @@
-const maplink = document.querySelector(".contacts-button-map");
-const mappopup = document.querySelector(".modal-map");
-const mapclose = document.querySelector(".modal-close-map");
+(function(){
+var maplink = document.querySelector(".contacts-button-map");
+var mappopup = document.querySelector(".modal-map");
+var mapclose = document.querySelector(".modal-close-map");
+var overlayMapModal = document.querySelector('.modal-overlay-map');
 
-     maplink.addEventListener("click", function(evt){
+for(var i = 0; i < maplink.length; i++){
+
+    maplink[i].addEventListener('click',function(evt){
         evt.preventDefault();
-        mappopup.classList.add("modal-show")
-    });
-
-     mapclose.addEventListener("click",  function(evt){
-        evt.preventDefault();
-        mappopup.classList.remove("modal-show")
-    });
-
-    window.addEventListener("keydown",  function (evt) {
-        if(evt.keyCode === 27) {
-            if(mappopup.classList.contains("modal-show")){
-                evt.preventDefault();
-                mappopup.classList.remove("modal-show")
-            }
-        }
+        mappopup.classList.add('modal-show');
+        overlayMapModal.classList.add('overlay-show');
     })
+}
+
+mapclose.addEventListener("click",  function(evt){
+    evt.preventDefault();
+    mappopup.classList.remove("modal-show")
+    overlayMapModal.classList.remove('overlay-show');
+    });
+
+overlayMapModal.addEventListener('click',function(event){
+    mappopup.classList.remove('modal-show');
+    this.classList.remove('overlay-show');
+})
+
+    maplink.addEventListener("click", function(evt){
+        evt.preventDefault();
+        mappopup.classList.add("modal-show");
+        });
+    
+    document.addEventListener('keydown',function(evt){
+		if(evt.keyCode === 27){
+			mappopup.classList.remove('modal-show');
+			overlayMapModal.classList.remove('overlay-show');
+		}
+	})
+})()
